19aaedc6b8900ae67a5151cb98e8fcaf85d4247e
Thomas Sonntag <git@sonntagsbox.de>
2012-12-10 15:34:42 +0100

    fixed autoload for Application

--- lib/init.rb
+++ lib/init.rb
@@ -2,14 +2,13 @@ dir = File.dirname(__FILE__)
 $LOAD_PATH.unshift dir unless $LOAD_PATH.include?(dir)
 
 module Init
-  autoload :AbstractItem, 'init/abstract_item'
-  autoload :ThreadItem, 'init/thread_item'
-  autoload :ProcessItem, 'init/process_item'
-  autoload :Init, 'init/init'
-  autoload :Client, 'init/client'
-  autoload :AbstractServer, 'init/abstract_server'
-  autoload :ProcessServer, 'init/process_server'
-  autoload :ThreadServer, 'init/thread_server'
-  autoload :Periodic, 'init/periodic'
-  autoload :Stoppable, 'init/stoppable'
+  autoload :AbstractItem,    'init/abstract_item'
+  autoload :ThreadItem,      'init/thread_item'
+  autoload :ProcessItem,     'init/process_item'
+  autoload :Client,          'init/client'
+  autoload :AbstractServer,  'init/abstract_server'
+  autoload :ProcessServer,   'init/process_server'
+  autoload :ThreadServer,    'init/thread_server'
+  autoload :Periodic,        'init/periodic'
+  autoload :Application,     'init/application'
 end
